d158223795c6a29be9e47b6bcc4bb2bb70f14432
djeytkey <tarik.engineering@gmail.com>
2021-12-28 00:08:27 +0100

    users and permissions

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = \App\User::create([
+            'first_name' => 'super',
+            'last_name' => 'admin',
+            'email' => 'super_admin@yovente.com',
+            'password' => bcrypt('123456'),
+            'phone' => '0689385061',
+            'is_active' => '1',
+            'is_deleted' => '1',
+        ]);
+
+        $user->attachRole('super_admin');
+    }
+}
